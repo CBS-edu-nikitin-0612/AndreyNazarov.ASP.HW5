65abbe0642026783e3d2de289fcebb24fb56ef0b
Andrew Nazarov <xiss@users.noreply.github.com>
2022-07-31 15:59:47 +0300

    Add project files.

---
+++ Task1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private static int _users;
+		public IActionResult Index()			
+		{
+			_users++;
+			return View( _users);
+		}
+	}
+}
---
+++ Task2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Task2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(string value, DateTime date)
+		{
+			CookieOptions options = new CookieOptions();
+			options.Expires = date;
+			Response.Cookies.Append("test", value, options);
+
+			return View();
+		}
+
+		public IActionResult Test()
+		{
+			if (Request.Cookies["test"] != null)
+			{
+				ViewBag.CookieExist = true;
+				ViewBag.Value = Request.Cookies["test"];
+			}
+			else
+			{
+				ViewBag.CookieExist = false;
+			}
+			return View();
+		}
+	}
+}
